ac81b97ad8101d90c9d282b1996e8f6efb6ca131
Ben Meadors <ben.p.meadors@gmail.com>
2023-05-25 17:48:25 -0400

    updated exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,42 +1,22 @@
 package shapes;
 
-public class Rectangle {
-    protected int length;
-    protected int width;
+public class Rectangle extends Quadrilateral implements Measurable {
 
-    public int getLength() {
-        return length;
+    protected double length;
+    protected double width;
+    @Override
+    public double getPerimeter() {
+        return ((2* length) + (2 * width));
     }
 
-    public int getWidth() {
-        return width;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public Rectangle(int length, int width){
-        this.length = length;
-        this.width = width;
-    }
-
-    public int getArea(){
+    @Override
+    public double getArea() {
         return length * width;
     }
 
-    public int getPerimeter(){
-        return(2 * length) + (2 * width);
+    public Rectangle(double length, double width){
+        super(length, width);
+        this.width = width;
+        this.length = length;
     }
-
-    public static void main(String[] args) {
-        Rectangle nRec = new Rectangle(2,3);
-
-        System.out.println();
-    }
-
 }
